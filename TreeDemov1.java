b4428e542193b9ee47b97d8bd30464a98c961358
Caden <binderca@cwu.edu>
2024-10-09 14:29:42 -0700

    Second version with the inOrderTraversal method

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -1,18 +1,18 @@
 class Node{
-	   int value;
-	   Node left, right;
+   int value;
+   Node left, right;
 	   
-	   public Node(int value){
-	      this.value = value;
-	      left = null;
-	      right = null;
-	   }
+   public Node(int value){
+      this.value = value;
+      left = null;
+      right = null;
+   }
 
-	}
+}
 
-	class BinarySearchTree{
+class BinarySearchTree{
 
-	   Node root;
+   Node root;
 	  
 	   
 	   /*
@@ -22,36 +22,36 @@ class Node{
 	   /*
 	   inserts a node into the tree
 	   */
-	   public void insert(int value){
-	      //tree is empty
-	      if(root == null){
-	         root = new Node(value);
-	         return;
-	      }else{
-	         Node current = root;
-	         Node parent = null;
-	         
-	         while(true){
-	            parent = current;
-	            
-	            if(value < current.value){
-	               current = current.left;
-	               if(current == null){
-	                  parent.left = new Node(value);
-	                  return;
-	               }
-	            }else{
-	               current = current.right;
-	               if(current == null){
-	                  parent.right = new Node(value);
-	                  return;
-	               }
-	            }
-	           
-	         }//closing while
-	      
-	      }//closing main if-else 
-	   }
+   public void insert(int value){
+         //tree is empty
+      if(root == null){
+         root = new Node(value);
+         return;
+      }else{
+         Node current = root;
+         Node parent = null;
+            
+         while(true){
+            parent = current;
+               
+            if(value < current.value){
+               current = current.left;
+               if(current == null){
+                  parent.left = new Node(value);
+                  return;
+               }
+            }else{
+               current = current.right;
+               if(current == null){
+                  parent.right = new Node(value);
+                  return;
+               }
+            }
+              
+         }//closing while
+         
+      }//closing main if-else 
+   }
 	   
 	/*
    pre-order traversal
@@ -71,12 +71,23 @@ class Node{
       }
    }
 	   
-	   /*
-	   in-order traversal
-	   */
-	   public void inOrderTraversal(Node root){
-	      //implement in here
-	   }
+	/*
+	in-order traversal
+   Prints the value of every node in order traversal. Uses the root.left and root.right 
+   functions to go through the in order traversal. 
+	*/
+   public void inOrderTraversal(Node root) { 
+   
+      if (root != null) { 
+      
+         inOrderTraversal(root.left); 
+         
+         System.out.print(root.value + " "); 
+         
+         inOrderTraversal(root.right); 
+         
+      }
+   }
 	   
 	   
 	   
@@ -84,10 +95,10 @@ class Node{
 	   post-order traversal
 	   */
 	  
-	   public void postOrderTraversal(Node root){
+   public void postOrderTraversal(Node root){
          //implement in here
-		   
-	   }
+   	   
+   }
 	   
 	   
 	   
@@ -95,10 +106,10 @@ class Node{
 	   a method to find the node in the tree
 	   with a specific value
 	   */
-	   public boolean find(Node root, int key){
-		   //implement in here
-		   return true;
-	   }
+   public boolean find(Node root, int key){
+   	   //implement in here
+      return true;
+   }
 	   
 	   
 	   
@@ -106,10 +117,10 @@ class Node{
 	   a method to find the node in the tree
 	   with a smallest key
 	   */
-	   public int getMin(Node root){
+   public int getMin(Node root){
          //implement in here
-	      return 0;
-	   }
+      return 0;
+   }
 	  
 	  
 	  
@@ -117,10 +128,10 @@ class Node{
 	   a method to find the node in the tree
 	   with a largest key
 	   */
-	   public int getMax(Node root){
+   public int getMax(Node root){
          //implement in here
-         return 0;
-	   }
+      return 0;
+   }
 	   
 	   
 	   
@@ -128,54 +139,54 @@ class Node{
 	   this method will not compile until getMax
 	   is implemented
 	   */
-	   public Node delete(Node root, int key){
-	      
-	      if(root == null){
-	         return root;
-	      }else if(key < root.value){
-	         root.left = delete(root.left, key);
-	      }else if(key > root.value){
-	         root.right = delete(root.right, key);
-	      }else{
-	         //node has been found
-	         if(root.left==null && root.right==null){
-	            //case #1: leaf node
-	            root = null;
-	         }else if(root.right == null){
-	            //case #2 : only left child
-	            root = root.left;
-	         }else if(root.left == null){
-	            //case #2 : only right child
-	            root = root.right;
-	         }else{
-	            //case #3 : 2 children
-	            root.value = getMax(root.left);
-	            root.left = delete(root.left, root.value);
-	         }
-	      }
-	      return root;  
-	   }
+   public Node delete(Node root, int key){
+         
+      if(root == null){
+         return root;
+      }else if(key < root.value){
+         root.left = delete(root.left, key);
+      }else if(key > root.value){
+         root.right = delete(root.right, key);
+      }else{
+            //node has been found
+         if(root.left==null && root.right==null){
+               //case #1: leaf node
+            root = null;
+         }else if(root.right == null){
+               //case #2 : only left child
+            root = root.left;
+         }else if(root.left == null){
+               //case #2 : only right child
+            root = root.right;
+         }else{
+               //case #3 : 2 children
+            root.value = getMax(root.left);
+            root.left = delete(root.left, root.value);
+         }
+      }
+      return root;  
+   }
 	   
 	   
 	   
-	}
+}
 
 
 
-	public class TreeDemov1{
-	   public static void main(String[] args){
-	      BinarySearchTree t1  = new BinarySearchTree();
-	      t1.insert( 24);
-	      t1.insert(80);
-	      t1.insert(18);
-	      t1.insert(9);
-	      t1.insert(90);
-	      t1.insert(22);
-	            
-	      System.out.print("in-order :   ");
-	      t1.inOrderTraversal(t1.root);
-	      System.out.println();
-	           
-	      
-	   }  
-	}
+public class TreeDemov1{
+   public static void main(String[] args){
+      BinarySearchTree t1  = new BinarySearchTree();
+      t1.insert( 24);
+      t1.insert(80);
+      t1.insert(18);
+      t1.insert(9);
+      t1.insert(90);
+      t1.insert(22);
+               
+      System.out.print("in-order :   ");
+      t1.inOrderTraversal(t1.root);
+      System.out.println();
+              
+         
+   }  
+}
